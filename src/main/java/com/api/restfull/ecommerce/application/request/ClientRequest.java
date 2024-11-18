2953f43c0c55585bb45f45834f1e83990007351e
Nailton2002 <jnmp25jnmp@gmail.com>
2024-11-18 09:13:42 -0300

    Ajuste nos DTOs de clientes

--- src/main/java/com/api/restfull/ecommerce/application/request/ClientRequest.java
+++ src/main/java/com/api/restfull/ecommerce/application/request/ClientRequest.java
@@ -1,50 +1,41 @@
 package com.api.restfull.ecommerce.application.request;
 
-import com.api.restfull.ecommerce.domain.model.Address;
+import com.api.restfull.ecommerce.application.dto.AdressDto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ClientRequest{
-        @NotBlank(message = "O nome é obrigatório.")
-        @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres.")
-        @Column(nullable = false)
-        private String nome;
-        @Email(message = "Formato do email é inválido")
-        @NotBlank(message = "Email é obrigatório")
-        private String email;
-        @CPF(message = "CPF inválido")
-        @NotBlank(message = "CPF é obrigatório")
-        private String cpf;
-        @NotNull(message = "A data de nascimento é obrigatória.")
-        @Past(message = "A data de nascimento deve estar no passado.")
-        @JsonFormat(pattern = "dd/MM/yyyy")
-        private LocalDate dataNascimento;
-        @Column(nullable = false)
-        @NotBlank(message = "Telefone é obrigatório")
-        @Pattern(regexp = "\\d{10,11}", message = "O telefone deve conter 10 ou 11 dígitos.")
-        private String telefone;
-        @Enumerated(EnumType.STRING)
-        @Column(nullable = false)
-        private boolean ativo;
-        @Valid @NotNull(message = "Dados do endereço são obrigatórios")
-        private Address endereco;
+public record ClientRequest(@NotBlank(message = "O nome é obrigatório.")
+                            @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres.")
+                            @Column(nullable = false)
+                            String nome,
+                            @Email(message = "Formato do email é inválido")
+                            @NotBlank(message = "Email é obrigatório")
+                            String email,
+                            @CPF(message = "CPF inválido")
+                            @NotBlank(message = "CPF é obrigatório")
+                            @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")
+                            String cpf,
+                            @NotNull(message = "A data de nascimento é obrigatória.")
+                            @Past(message = "A data de nascimento deve estar no passado.")
+                            @JsonFormat(pattern = "dd/MM/yyyy")
+                            LocalDate dataNascimento,
+                            @Column(nullable = false)
+                            @NotBlank(message = "Telefone é obrigatório")
+                            @Pattern(regexp = "\\d{10,11}", message = "O telefone deve conter 10 ou 11 dígitos.")
+                            String telefone,
+                            @Enumerated(EnumType.STRING)
+                            @Column(nullable = false)
+                            boolean ativo,
+                            @Valid @NotNull(message = "Dados do endereço são obrigatórios")
+                            AdressDto endereco) {
 
 
 }
+
--- src/main/java/com/api/restfull/ecommerce/application/response/ClientResponse.java
+++ src/main/java/com/api/restfull/ecommerce/application/response/ClientResponse.java
@@ -1,32 +1,16 @@
 package com.api.restfull.ecommerce.application.response;
 
-import com.api.restfull.ecommerce.domain.model.Address;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.br.CPF;
+import com.api.restfull.ecommerce.application.dto.AdressDto;
 
 import java.time.LocalDate;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ClientResponse {
-    private Long id;
-    private String nome;
-    private String email;
-    private String cpf;
-    private LocalDate dataNascimento;
-    private String telefone;
-    private boolean ativo;
-    private Address endereco;
+public record ClientResponse(Long id,
+                             String nome,
+                             String email,
+                             String cpf,
+                             LocalDate dataNascimento,
+                             String telefone,
+                             Boolean ativo,
+                             AdressDto endereco) {
+
 }
