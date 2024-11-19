a7fc3e5c25dcf23593329d2d6202f5ada804e015
Nailton2002 <jnmp25jnmp@gmail.com>
2024-11-18 22:02:13 -0300

    Criando as regras de negocio de produtos

--- src/main/java/com/api/restfull/ecommerce/application/service/ProductService.java
+++ src/main/java/com/api/restfull/ecommerce/application/service/ProductService.java
@@ -1,10 +1,9 @@
 package com.api.restfull.ecommerce.application.service;
 
-import com.api.restfull.ecommerce.application.request.CategoryRequest;
 import com.api.restfull.ecommerce.application.request.ProductRequest;
-import com.api.restfull.ecommerce.application.response.CategoryResponse;
 import com.api.restfull.ecommerce.application.response.ProductResponse;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,20 +12,44 @@ import java.util.List;
 public interface ProductService {
 
     @Transactional
-    ProductResponse save(ProductRequest request);
+    ProductResponse createProduct(ProductRequest request);
 
     @Transactional
-    ProductResponse update(Long id, ProductRequest request);
+    ProductResponse getByIdProduct(Long id);
 
     @Transactional
-    ProductResponse findByNameProduct(String nome);
+    Page<ProductResponse> getAllPagedProducts(int page, int size);
+
+    @Transactional
+    ProductResponse updateProduct(ProductRequest request);
+
+    @Transactional
+    List<ProductResponse> getAllByParamProducts(Long id_cat);
+
+    @Transactional
+    List<ProductResponse> getAllByParamNameProducts(String nomeCategoria);
+
+    @Transactional
+    List<ProductResponse> searchByName(String nome);
+
+    @Transactional
+   ProductResponse getByNameProduct(String nome);
+
+    @Transactional
+    List<ProductResponse> getByDescriptionProduct(String descricao);
+
+    @Transactional
+    List<ProductResponse> getSortedByPriceProducts(String order);
+
+    @Transactional
+    List<ProductResponse> getByQuantityProduct(Integer quantidadeEstoque);
 
     @Transactional
     ProductResponse desableProduct(Long id);
 
-    ProductResponse findById(Long id);
+    @Transactional
+    List<ProductResponse> getByStatusProducts(Boolean ativo);
 
-    List<ProductResponse> findAll();
-
-    void deleteCategoryDesable(Long id);
+    @Transactional
+    void deleteProduct(Long id);
 }
